61ef7c47bf6de2b4747c81c0ec04e4fa1c7064be
robmadeyou <robmadeyou@gmail.com>
2017-01-23 15:03:47 +0000

    Renaming Title to be Subject

--- src/Leaves/CommunicationItem/CommunicationItemCollectionView.php
+++ src/Leaves/CommunicationItem/CommunicationItemCollectionView.php
@@ -50,7 +50,7 @@ class CommunicationItemCollectionView extends View
 
         $table->columns = [
             '#' => 'CommunicationItemID',
-            'Title' => '{Title}',
+            'Subject' => '{Title}',
             'Recipient',
             'DateCreated',
             'Date to Send' => new DateColumn('DateToSend', 'Date to Send', CommunicationDecorator::DATE_FORMAT),
